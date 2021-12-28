6f36a700a96f19bcead8d234c9a910b695b3eea8
Abhishek Sharma <abhishekbhardwaj540@gmail.com>
2021-12-28 13:00:01 -0500

    Making changes due to the repository

--- scripts/tutorial_configuration_setup.sh
+++ scripts/tutorial_configuration_setup.sh
@@ -55,7 +55,7 @@ done
 ssh -o StrictHostKeyChecking=no ${username}@${controller_node} "
   # Clone this repository to get the experiment configuration files.
   ssh-keygen -F github.com || ssh-keyscan github.com >> ~/.ssh/known_hosts
-  git clone https://github.com/rodrigoalveslima/BuzzBlogBenchmark.git -b highThputExp
+  git clone https://github.com/unkemptArc99/BuzzBlogBenchmark-1.git BuzzBlogBenchmark -b highThputExp
   mv BuzzBlogBenchmark/controller/conf/tutorial/* .
   rm -rf BuzzBlogBenchmark
 
