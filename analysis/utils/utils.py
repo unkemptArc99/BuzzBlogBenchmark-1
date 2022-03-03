71f023ee0a335509c05e19795c284cac4311b07c
Rodrigo A. Lima <rodrigoalveslima@gmail.com>
2022-03-03 06:08:03 +0000

    Refactor utilities.

--- analysis/utils/utils.py
+++ analysis/utils/utils.py
@@ -1,229 +1,146 @@
+# Copyright (C) 2020 Georgia Tech Center for Experimental Research in Computer
+# Systems
+
 import gzip
 import os
 import pandas as pd
-import re
 import sys
 import tarfile
 
 sys.path.append(os.path.abspath(os.path.join("..")))
-from parsers.loadgen_parser import LoadgenParser
+from parsers import collectl_parser, loadgen_parser, query_parser, redis_parser, rpc_parser
 
 
-def get_node_names(experiment_dirname):
+def get_node_names(experiment_dirpath):
     return [dirname
-            for dirname in os.listdir(os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs"))
+            for dirname in os.listdir(os.path.join(experiment_dirpath, "logs"))
             if not dirname.startswith('.')]
 
 
-def get_rpc_csvfiles(experiment_dirname):
-    tarball_patterns = [
-        r"^apigateway.*\.tar\.gz$",
-        r"^.+_service.*\.tar\.gz$",
-    ]
-    for node_name in get_node_names(experiment_dirname):
-        for tarball_name in os.listdir(os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname,
-                "logs",  node_name)):
-            if sum([1 if re.match(tarball_pattern, tarball_name) else 0 for tarball_pattern in tarball_patterns]):
-                tarball_path = os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs",
-                        node_name, tarball_name)
-                with tarfile.open(tarball_path, "r:gz") as tar:
-                    for filename in tar.getnames():
-                        if filename.endswith("calls.csv"):
-                            with tar.extractfile(filename) as csvfile:
-                                yield csvfile
-
-
-def get_rpc_logfiles(experiment_dirname):
-    tarball_patterns = [
-        r"^apigateway.*\.tar\.gz$",
-        r"^.+_service.*\.tar\.gz$",
-    ]
-    for node_name in get_node_names(experiment_dirname):
-        for tarball_name in os.listdir(os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname,
-                "logs",  node_name)):
-            if sum([1 if re.match(tarball_pattern, tarball_name) else 0 for tarball_pattern in tarball_patterns]):
-                tarball_path = os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs",
-                        node_name, tarball_name)
-                with tarfile.open(tarball_path, "r:gz") as tar:
+def get_rpc_df(experiment_dirpath):
+    for node_name in get_node_names(experiment_dirpath):
+        for tarball_name in os.listdir(os.path.join(experiment_dirpath, "logs", node_name)):
+            tarball_path = os.path.join(experiment_dirpath, "logs", node_name, tarball_name)
+            with tarfile.open(tarball_path, "r:gz") as tar:
+                for filename in tar.getnames():
+                    if filename.endswith("calls.csv"):
+                        with tar.extractfile(filename) as csvfile:
+                            yield (node_name, tarball_name, pd.read_csv(csvfile, parse_dates=["timestamp"]))
+                            break
+                else:
                     for filename in tar.getnames():
                         if filename.endswith("calls.log"):
                             with tar.extractfile(filename) as logfile:
-                                yield logfile
+                                yield (node_name, tarball_name, rpc_parser.RPCParser.df(logfile))
 
 
-def get_query_csvfiles(experiment_dirname):
-    tarball_patterns = [
-        r"^.+_service.*\.tar\.gz$",
-    ]
-    for node_name in get_node_names(experiment_dirname):
-        for tarball_name in os.listdir(os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname,
-                "logs", node_name)):
-            if sum([1 if re.match(tarball_pattern, tarball_name) else 0 for tarball_pattern in tarball_patterns]):
-                tarball_path = os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs",
-                        node_name, tarball_name)
-                with tarfile.open(tarball_path, "r:gz") as tar:
-                    for filename in tar.getnames():
-                        if filename.endswith("queries.csv"):
-                            with tar.extractfile(filename) as csvfile:
-                                yield csvfile
-
-
-def get_query_logfiles(experiment_dirname):
-    tarball_patterns = [
-        r"^.+_service.*\.tar\.gz$",
-    ]
-    for node_name in get_node_names(experiment_dirname):
-        for tarball_name in os.listdir(os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname,
-                "logs", node_name)):
-            if sum([1 if re.match(tarball_pattern, tarball_name) else 0 for tarball_pattern in tarball_patterns]):
-                tarball_path = os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs",
-                        node_name, tarball_name)
-                with tarfile.open(tarball_path, "r:gz") as tar:
+def get_query_df(experiment_dirpath):
+    for node_name in get_node_names(experiment_dirpath):
+        for tarball_name in os.listdir(os.path.join(experiment_dirpath, "logs", node_name)):
+            tarball_path = os.path.join(experiment_dirpath, "logs", node_name, tarball_name)
+            with tarfile.open(tarball_path, "r:gz") as tar:
+                for filename in tar.getnames():
+                    if filename.endswith("queries.csv"):
+                        with tar.extractfile(filename) as csvfile:
+                            yield (node_name, tarball_name, pd.read_csv(csvfile, parse_dates=["timestamp"]))
+                            break
+                else:
                     for filename in tar.getnames():
                         if filename.endswith("queries.log"):
                             with tar.extractfile(filename) as logfile:
-                                yield logfile
+                                yield (node_name, tarball_name, query_parser.QueryParser.df(logfile))
 
 
-def get_redis_csvfiles(experiment_dirname):
-    tarball_patterns = [
-        r"^.+_service.*\.tar\.gz$",
-    ]
-    for node_name in get_node_names(experiment_dirname):
-        for tarball_name in os.listdir(os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname,
-                "logs", node_name)):
-            if sum([1 if re.match(tarball_pattern, tarball_name) else 0 for tarball_pattern in tarball_patterns]):
-                tarball_path = os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs",
-                        node_name, tarball_name)
-                with tarfile.open(tarball_path, "r:gz") as tar:
-                    for filename in tar.getnames():
-                        if filename.endswith("redis.csv"):
-                            with tar.extractfile(filename) as csvfile:
-                                yield csvfile
-
-
-def get_redis_logfiles(experiment_dirname):
-    tarball_patterns = [
-        r"^.+_service.*\.tar\.gz$",
-    ]
-    for node_name in get_node_names(experiment_dirname):
-        for tarball_name in os.listdir(os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname,
-                "logs", node_name)):
-            if sum([1 if re.match(tarball_pattern, tarball_name) else 0 for tarball_pattern in tarball_patterns]):
-                tarball_path = os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs",
-                        node_name, tarball_name)
-                with tarfile.open(tarball_path, "r:gz") as tar:
+def get_redis_df(experiment_dirpath):
+    for node_name in get_node_names(experiment_dirpath):
+        for tarball_name in os.listdir(os.path.join(experiment_dirpath, "logs", node_name)):
+            tarball_path = os.path.join(experiment_dirpath, "logs", node_name, tarball_name)
+            with tarfile.open(tarball_path, "r:gz") as tar:
+                for filename in tar.getnames():
+                    if filename.endswith("redis.csv"):
+                        with tar.extractfile(filename) as csvfile:
+                            yield (node_name, tarball_name, pd.read_csv(csvfile, parse_dates=["timestamp"]))
+                            break
+                else:
                     for filename in tar.getnames():
                         if filename.endswith("redis.log"):
                             with tar.extractfile(filename) as logfile:
-                                yield logfile
+                                yield (node_name, tarball_name, redis_parser.RedisParser.df(logfile))
 
 
-def get_loadgen_csvfiles(experiment_dirname):
-    for node_name in get_node_names(experiment_dirname):
-        for tarball_name in os.listdir(os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname,
-                "logs",  node_name)):
-            if re.match(r"^loadgen.*\.tar\.gz$", tarball_name):
-                tarball_path = os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs",
-                        node_name, tarball_name)
-                with tarfile.open(tarball_path, "r:gz") as tar:
-                    for filename in tar.getnames():
-                        if filename.endswith("loadgen.csv"):
-                            with tar.extractfile(filename) as csvfile:
-                                yield csvfile
-
-
-def get_loadgen_logfiles(experiment_dirname):
-    for node_name in get_node_names(experiment_dirname):
-        for tarball_name in os.listdir(os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname,
-                "logs",  node_name)):
-            if re.match(r"^loadgen.*\.tar\.gz$", tarball_name):
-                tarball_path = os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs",
-                        node_name, tarball_name)
-                with tarfile.open(tarball_path, "r:gz") as tar:
+def get_loadgen_df(experiment_dirpath):
+    for node_name in get_node_names(experiment_dirpath):
+        for tarball_name in os.listdir(os.path.join(experiment_dirpath, "logs", node_name)):
+            tarball_path = os.path.join(experiment_dirpath, "logs", node_name, tarball_name)
+            with tarfile.open(tarball_path, "r:gz") as tar:
+                for filename in tar.getnames():
+                    if filename.endswith("loadgen.csv"):
+                        with tar.extractfile(filename) as csvfile:
+                            yield (node_name, tarball_name, pd.read_csv(csvfile, parse_dates=["timestamp"]))
+                            break
+                else:
                     for filename in tar.getnames():
                         if filename.endswith("loadgen.log"):
                             with tar.extractfile(filename) as logfile:
-                                yield logfile
+                                yield (node_name, tarball_name, loadgen_parser.LoadgenParser.df(logfile))
 
 
-def get_collectl_cpu_csvfiles(experiment_dirname):
-    for node_name in get_node_names(experiment_dirname):
-        tarball_path = os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs", node_name,
-                "collectl.tar.gz")
-        with tarfile.open(tarball_path, "r:gz") as tar:
-            for filename in tar.getnames():
-                if filename.endswith(".cpu.csv"):
-                    with tar.extractfile(filename) as csvfile:
-                        yield (node_name, csvfile)
+def get_collectl_cpu_df(experiment_dirpath):
+    for node_name in get_node_names(experiment_dirpath):
+        for tarball_name in os.listdir(os.path.join(experiment_dirpath, "logs", node_name)):
+            tarball_path = os.path.join(experiment_dirpath, "logs", node_name, tarball_name)
+            with tarfile.open(tarball_path, "r:gz") as tar:
+                for filename in tar.getnames():
+                    if filename.endswith(".cpu.csv"):
+                        with tar.extractfile(filename) as csvfile:
+                            yield (node_name, tarball_name, pd.read_csv(csvfile, parse_dates=["timestamp"]))
+                            break
+                else:
+                    for filename in tar.getnames():
+                        if filename.endswith(".cpu.gz"):
+                            with gzip.open(tar.extractfile(filename), "rt") as logfile:
+                                yield (node_name, tarball_name, collectl_parser.CollectlParser.df(logfile, "cpu"))
 
 
-def get_collectl_cpu_logfiles(experiment_dirname):
-    for node_name in get_node_names(experiment_dirname):
-        tarball_path = os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs", node_name,
-                "collectl.tar.gz")
-        with tarfile.open(tarball_path, "r:gz") as tar:
-            for filename in tar.getnames():
-                if filename.endswith(".cpu.gz"):
-                    with gzip.open(tar.extractfile(filename), "rt") as logfile:
-                        yield (node_name, logfile)
+def get_collectl_mem_df(experiment_dirpath):
+    for node_name in get_node_names(experiment_dirpath):
+        for tarball_name in os.listdir(os.path.join(experiment_dirpath, "logs", node_name)):
+            tarball_path = os.path.join(experiment_dirpath, "logs", node_name, tarball_name)
+            with tarfile.open(tarball_path, "r:gz") as tar:
+                for filename in tar.getnames():
+                    if filename.endswith(".numa.csv"):
+                        with tar.extractfile(filename) as csvfile:
+                            yield (node_name, tarball_name, pd.read_csv(csvfile, parse_dates=["timestamp"]))
+                            break
+                else:
+                    for filename in tar.getnames():
+                        if filename.endswith(".numa.gz"):
+                            with gzip.open(tar.extractfile(filename), "rt") as logfile:
+                                yield (node_name, tarball_name, collectl_parser.CollectlParser.df(logfile, "mem"))
 
 
-def get_collectl_mem_csvfiles(experiment_dirname):
-    for node_name in get_node_names(experiment_dirname):
-        tarball_path = os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs", node_name,
-                "collectl.tar.gz")
-        with tarfile.open(tarball_path, "r:gz") as tar:
-            for filename in tar.getnames():
-                if filename.endswith(".numa.csv"):
-                    with tar.extractfile(filename) as csvfile:
-                        yield (node_name, csvfile)
+def get_collectl_dsk_df(experiment_dirpath):
+    for node_name in get_node_names(experiment_dirpath):
+        for tarball_name in os.listdir(os.path.join(experiment_dirpath, "logs", node_name)):
+            tarball_path = os.path.join(experiment_dirpath, "logs", node_name, tarball_name)
+            with tarfile.open(tarball_path, "r:gz") as tar:
+                for filename in tar.getnames():
+                    if filename.endswith(".dsk.csv"):
+                        with tar.extractfile(filename) as csvfile:
+                            yield (node_name, tarball_name, pd.read_csv(csvfile, parse_dates=["timestamp"]))
+                            break
+                else:
+                    for filename in tar.getnames():
+                        if filename.endswith(".dsk.gz"):
+                            with gzip.open(tar.extractfile(filename), "rt") as logfile:
+                                yield (node_name, tarball_name, collectl_parser.CollectlParser.df(logfile, "dsk"))
 
 
-def get_collectl_mem_logfiles(experiment_dirname):
-    for node_name in get_node_names(experiment_dirname):
-        tarball_path = os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs", node_name,
-                "collectl.tar.gz")
-        with tarfile.open(tarball_path, "r:gz") as tar:
-            for filename in tar.getnames():
-                if filename.endswith(".numa.gz"):
-                    with gzip.open(tar.extractfile(filename), "rt") as logfile:
-                        yield (node_name, logfile)
-
-
-def get_collectl_dsk_csvfiles(experiment_dirname):
-    for node_name in get_node_names(experiment_dirname):
-        tarball_path = os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs", node_name,
-                "collectl.tar.gz")
-        with tarfile.open(tarball_path, "r:gz") as tar:
-            for filename in tar.getnames():
-                if filename.endswith(".dsk.csv"):
-                    with tar.extractfile(filename) as csvfile:
-                        yield (node_name, csvfile)
-
-
-def get_collectl_dsk_logfiles(experiment_dirname):
-    for node_name in get_node_names(experiment_dirname):
-        tarball_path = os.path.join(os.path.dirname(__file__), "..", "data", experiment_dirname, "logs", node_name,
-                "collectl.tar.gz")
-        with tarfile.open(tarball_path, "r:gz") as tar:
-            for filename in tar.getnames():
-                if filename.endswith(".dsk.gz"):
-                    with gzip.open(tar.extractfile(filename), "rt") as logfile:
-                        yield (node_name, logfile)
-
-
-def get_experiment_start_time(experiment_dirname):
-    requests = pd.concat([
-            pd.DataFrame.from_dict(LoadgenParser(logfile).parse())
-            for logfile in get_loadgen_logfiles(experiment_dirname)
-    ], ignore_index=True)
+def get_experiment_start_time(experiment_dirpath):
+    requests = pd.concat([df[2] for df in get_loadgen_df(experiment_dirpath)])
     return requests["timestamp"].values.min()
 
 
-def get_experiment_end_time(experiment_dirname):
-    requests = pd.concat([
-            pd.DataFrame.from_dict(LoadgenParser(logfile).parse())
-            for logfile in get_loadgen_logfiles(experiment_dirname)
-    ], ignore_index=True)
+def get_experiment_end_time(experiment_dirpath):
+    requests = pd.concat([df[2] for df in get_loadgen_df(experiment_dirpath)])
     return requests["timestamp"].values.max()
