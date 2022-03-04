b5d8ce5a8cbd0259b27b3704c021c2c453eb1757
Rodrigo A. Lima <rodrigoalveslima@gmail.com>
2022-03-04 06:07:38 +0000

    Add node name column to dataframes in utils methods.

--- analysis/utils/utils.py
+++ analysis/utils/utils.py
@@ -25,13 +25,15 @@ def get_rpc_df(experiment_dirpath):
                 for filename in tar.getnames():
                     if filename.endswith("calls.csv"):
                         with tar.extractfile(filename) as csvfile:
-                            yield (node_name, tarball_name, pd.read_csv(csvfile, parse_dates=["timestamp"]))
+                            yield (node_name, tarball_name,
+                                    pd.read_csv(csvfile, parse_dates=["timestamp"]).assign(node_name=node_name))
                             break
                 else:
                     for filename in tar.getnames():
                         if filename.endswith("calls.log"):
                             with tar.extractfile(filename) as logfile:
-                                yield (node_name, tarball_name, rpc_parser.RPCParser.df(logfile))
+                                yield (node_name, tarball_name,
+                                        rpc_parser.RPCParser.df(logfile).assign(node_name=node_name))
 
 
 def get_query_df(experiment_dirpath):
@@ -42,13 +44,15 @@ def get_query_df(experiment_dirpath):
                 for filename in tar.getnames():
                     if filename.endswith("queries.csv"):
                         with tar.extractfile(filename) as csvfile:
-                            yield (node_name, tarball_name, pd.read_csv(csvfile, parse_dates=["timestamp"]))
+                            yield (node_name, tarball_name,
+                                    pd.read_csv(csvfile, parse_dates=["timestamp"]).assign(node_name=node_name))
                             break
                 else:
                     for filename in tar.getnames():
                         if filename.endswith("queries.log"):
                             with tar.extractfile(filename) as logfile:
-                                yield (node_name, tarball_name, query_parser.QueryParser.df(logfile))
+                                yield (node_name, tarball_name,
+                                        query_parser.QueryParser.df(logfile).assign(node_name=node_name))
 
 
 def get_redis_df(experiment_dirpath):
@@ -59,13 +63,15 @@ def get_redis_df(experiment_dirpath):
                 for filename in tar.getnames():
                     if filename.endswith("redis.csv"):
                         with tar.extractfile(filename) as csvfile:
-                            yield (node_name, tarball_name, pd.read_csv(csvfile, parse_dates=["timestamp"]))
+                            yield (node_name, tarball_name,
+                                    pd.read_csv(csvfile, parse_dates=["timestamp"]).assign(node_name=node_name))
                             break
                 else:
                     for filename in tar.getnames():
                         if filename.endswith("redis.log"):
                             with tar.extractfile(filename) as logfile:
-                                yield (node_name, tarball_name, redis_parser.RedisParser.df(logfile))
+                                yield (node_name, tarball_name,
+                                        redis_parser.RedisParser.df(logfile).assign(node_name=node_name))
 
 
 def get_loadgen_df(experiment_dirpath):
@@ -76,13 +82,15 @@ def get_loadgen_df(experiment_dirpath):
                 for filename in tar.getnames():
                     if filename.endswith("loadgen.csv"):
                         with tar.extractfile(filename) as csvfile:
-                            yield (node_name, tarball_name, pd.read_csv(csvfile, parse_dates=["timestamp"]))
+                            yield (node_name, tarball_name,
+                                    pd.read_csv(csvfile, parse_dates=["timestamp"]).assign(node_name=node_name))
                             break
                 else:
                     for filename in tar.getnames():
                         if filename.endswith("loadgen.log"):
                             with tar.extractfile(filename) as logfile:
-                                yield (node_name, tarball_name, loadgen_parser.LoadgenParser.df(logfile))
+                                yield (node_name, tarball_name,
+                                        loadgen_parser.LoadgenParser.df(logfile).assign(node_name=node_name))
 
 
 def get_collectl_cpu_df(experiment_dirpath):
@@ -93,13 +101,15 @@ def get_collectl_cpu_df(experiment_dirpath):
                 for filename in tar.getnames():
                     if filename.endswith(".cpu.csv"):
                         with tar.extractfile(filename) as csvfile:
-                            yield (node_name, tarball_name, pd.read_csv(csvfile, parse_dates=["timestamp"]))
+                            yield (node_name, tarball_name,
+                                    pd.read_csv(csvfile, parse_dates=["timestamp"]).assign(node_name=node_name))
                             break
                 else:
                     for filename in tar.getnames():
                         if filename.endswith(".cpu.gz"):
                             with gzip.open(tar.extractfile(filename), "rt") as logfile:
-                                yield (node_name, tarball_name, collectl_parser.CollectlParser.df(logfile, "cpu"))
+                                yield (node_name, tarball_name,
+                                        collectl_parser.CollectlParser.df(logfile, "cpu").assign(node_name=node_name))
 
 
 def get_collectl_mem_df(experiment_dirpath):
@@ -110,13 +120,15 @@ def get_collectl_mem_df(experiment_dirpath):
                 for filename in tar.getnames():
                     if filename.endswith(".numa.csv"):
                         with tar.extractfile(filename) as csvfile:
-                            yield (node_name, tarball_name, pd.read_csv(csvfile, parse_dates=["timestamp"]))
+                            yield (node_name, tarball_name,
+                                    pd.read_csv(csvfile, parse_dates=["timestamp"]).assign(node_name=node_name))
                             break
                 else:
                     for filename in tar.getnames():
                         if filename.endswith(".numa.gz"):
                             with gzip.open(tar.extractfile(filename), "rt") as logfile:
-                                yield (node_name, tarball_name, collectl_parser.CollectlParser.df(logfile, "mem"))
+                                yield (node_name, tarball_name,
+                                        collectl_parser.CollectlParser.df(logfile, "mem").assign(node_name=node_name))
 
 
 def get_collectl_dsk_df(experiment_dirpath):
@@ -127,13 +139,15 @@ def get_collectl_dsk_df(experiment_dirpath):
                 for filename in tar.getnames():
                     if filename.endswith(".dsk.csv"):
                         with tar.extractfile(filename) as csvfile:
-                            yield (node_name, tarball_name, pd.read_csv(csvfile, parse_dates=["timestamp"]))
+                            yield (node_name, tarball_name,
+                                    pd.read_csv(csvfile, parse_dates=["timestamp"]).assign(node_name=node_name))
                             break
                 else:
                     for filename in tar.getnames():
                         if filename.endswith(".dsk.gz"):
                             with gzip.open(tar.extractfile(filename), "rt") as logfile:
-                                yield (node_name, tarball_name, collectl_parser.CollectlParser.df(logfile, "dsk"))
+                                yield (node_name, tarball_name,
+                                        collectl_parser.CollectlParser.df(logfile, "dsk").assign(node_name=node_name))
 
 
 def get_experiment_start_time(experiment_dirpath):
